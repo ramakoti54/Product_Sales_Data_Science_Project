80f47b81e70410bf0ef9676d0eccfdf97021113e
Ram T <44626534+ramakoti54@users.noreply.github.com>
2026-03-02 16:37:01 +0530

    Updation on Relu logic for Linear Regression
    Version 2.0

--- Rectified_Linear_Unit_API.py
+++ Rectified_Linear_Unit_API.py
@@ -1,31 +1,52 @@
+#**** Libraries of Python to import required modules *****#
+#**** Numpy, Pandas used to import Data types and variable components of python to perform Matrix operations for computing gradient and Dot product and Summation operations
+#**** Math library is to import required mathemetical opeartions like power, square root, loading dynamic math symbols to the class.
+#**** Matplot library demonstrates results of the ML Model prediction and Analysis processes through Diagrams and plots.
+
 import numpy as np;
 import pandas as pd;
 import matplotlib.pyplot as plt;
 import math;
 #import Analyse_Business_Revenue as ABR;
 
+#**** Defining Class to perform Rectified Linear Unit entropy computation on the Clothing product sales of FY24 for Zabra.
 class Rectified_Linear_Unit_API:
-   
+#**** Method defined to perform Linear regression model computation using the dot product operation on input matrix with data sample for both sales volume and price 
+#**** of the Clothes. The regression operation awaits for the arguments as Sales volume, Price for inputs and weight assumptions to compute resultant of the operation to
+#**** be displayed as input for the function Relu.   
    def  model(self,X,w):
       X = w[0] + np.dot(X,w[1:]);
       return(X);
+#**** Relu considers the inputs from Linear regression function and operates to calculate RELU(f(X)) using the exponential inverse function. The resultant consists of sales volumes
+#**** to be relied on Relu factor computation for the standard normalized sales volume and price values. The prediction process significantly relies on the Relu factor, weights associated
+#**** in the process.
 
    def Relu(self,X,w):
       
       X = 1/(1+(math.exp(self.model(X,w))))
       return(X);
-     
+   
+#**** Applying either of the two methods including Standard Normalizer, Gaussian Mean and Gaussian Range to find the Normalized imputed values, Limits of the Gaussian techniques to 
+#**** compute the resultant of Feature engineering principle applied inputs prior to sending the output to work as an argument for Traianing the model.
+#**** Standard Normalization process use Sales Volume, Price of the Clothes to compute updated Sales and Price values by adding the original value to it's Standard Deviation.
+#**** Gaussian Mean is a significant way of computing technique in feature engineering to be implemented on Quantitavie variables. The function assumes the numeric input vales for 
+#**** calculating Gaussian mean using the formula G(X) as described in the below calculation. The Limits allows the option to range the values for Gaussian Range function finding the 
+#**** difference between Maximum and Minimum values of Sales Volume and Price of the Zabra brand terms.
+
    def Standard_Normalization(self,Train_Dataset_Sales,Train_Dataset_Price,Product_Sales_List,Product_Price_List,i):
-      
+
+#**** Variable Declaration for storing the Mean computation values.
+#**** Input mehtod to be stored from the Dynamic Variable of I/O operation.      
       Normalized_Value = [];
       Product_Sales_Avg = 0.0;
-      method = str(input("Enter Method name for Performing Feature engineering method to impute sales and price of products either 1. Standard Normalization or 2. Gaussian Mean"))
+      method = str(input("Enter Method name for Performing Feature engineering method to impute sales and price of products either 1. Standard Normalization or 2. Gaussian Mean or 3. Gaussian Range"))
       #status = str(nput("Enter Status name as either Sales volume or Price computation for computing one at a time."))
       Normalized_Value = Train_Dataset_Sales;
+      Product_Sales_Range = np.max(Normalized_Value)-np.min(Normalized_Value);
       Normalized_Price_Value = Train_Dataset_Price;
+      Product_Price_Range = np.max(Normalized_Price_Value)-np.min(Normalized_Price_Value);
       for i in range(len(Normalized_Value)):
-         if(i < len(Normalized_Value)):
-            
+         if(i < len(Normalized_Value)):            
             if(method == "Standard Normalization" and Normalized_Value[i] != 'Nan'):
                Product_Normal_Avg = np.average(Normalized_Value);
                Product_Normal_Std = np.std(Normalized_Value);
@@ -42,6 +63,14 @@ class Rectified_Linear_Unit_API:
                Product_Normal_Avg = np.average(Normalized_Price_Value);
                Product_Normal_Std = np.std(Normalized_Price_Value);          
                Product_Price_List.append((1/math.sqrt(2*math.pi)*Product_Normal_Std)*math.pow(math.exp(-1*math.pow((Normalized_Price_Value[i]-Product_Normal_Avg),2))/(2*math.pow(Product_Normal_Std,2))));                
+            elif(method == "Gaussian Range" and Normalized_Value[i] != 'Nan'):
+               Product_Normal_Avg = np.average(Normalized_Value);
+               Product_Normal_Std = np.std(Normalized_Value);          
+               Product_Sales_List.append((1/math.sqrt(2*math.pi)*Product_Normal_Std)*math.pow(math.exp(-1*math.pow((Normalized_Value[i]-Product_Sales_Range),2))/(2*math.pow(Product_Normal_Std,2)))); 
+            elif(method == "Gaussian Range" and Normalized_Price_Value[i] != 'Nan'):   
+               Product_Normal_Avg = np.average(Normalized_Price_Value);
+               Product_Normal_Std = np.std(Normalized_Price_Value);          
+               Product_Price_List.append((1/math.sqrt(2*math.pi)*Product_Normal_Std)*math.pow(math.exp(-1*math.pow((Normalized_Price_Value[i]-Product_Price_Range),2))/(2*math.pow(Product_Normal_Std,2))));          
             else:
                continue;
          else:
@@ -50,11 +79,10 @@ class Rectified_Linear_Unit_API:
 
    def SNO_Generation(self,X,i,Dynamic_Number):
       for i in range(Dynamic_Number):
-         X.append(i);  
+         X.append(i+1);  
       return(X);   
 
-   def Scatter_Plot_Sales_Price(self,Product_Sales_Volume,Product_Price):
-      
+   def Scatter_Plot_Sales_Price(self,Product_Sales_Volume,Product_Price):      
       fig, ax = plt.subplots();
       ax.scatter(Product_Sales_Volume,Product_Price);
       plt.xlabel("Product Sales Volume");
